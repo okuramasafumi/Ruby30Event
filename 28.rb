7a38bb95c68054aaaf5c2eed6bdfd98bc398da47
OKURA Masafumi <masafumi.o1988@gmail.com>
2023-02-20 14:08:46 +0900

    Improve 28

--- 28.rb
+++ 28.rb
@@ -1,19 +1,18 @@
 # 文字列をバラしてから復元する
 ORDER = {
-  "H": 1,
-  "e": 2,
+  "H": [1],
+  "e": [2],
   "l": [3, 4, 11],
   "o": [5, 9],
-  ",": 6,
-  " ": 7,
-  "w": 8,
-  "r": 10,
-  "d": 12,
-  "!": 13
+  ",": [6],
+  " ": [7],
+  "w": [8],
+  "r": [10],
+  "d": [12],
+  "!": [13]
 }
 
-puts hello = "Hello, world!".chars.shuffle.each_with_object("a"*13) { |char, result|
+puts "Hello, world!".chars.shuffle.each_with_object("a"*13) { |char, result|
   index = ORDER[char.to_sym]
-  idx = index.is_a?(Array) ? index.shift : index
-  result[idx - 1] = char
+  result[index.shift - 1] = char
 }
