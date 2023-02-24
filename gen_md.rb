7ae9b421541d1ccfe6018e9938a9ddddd26b3ece
OKURA Masafumi <masafumi.o1988@gmail.com>
2023-02-24 23:19:34 +0900

    Katakana!

--- gen_md.rb
+++ gen_md.rb
@@ -9,14 +9,14 @@ f = File.open('slide.md', 'w') do |f|
   ---
   FRONTMATTER
   f << frontmatter
-  f << "# Ruby30\nRubyでHello worldする30の方法\n"
+  f << "# Ruby30\nRubyでハローワールドする30の方法\n"
   f << "\n"
   f << "---\n"
   f << "## i = 30\n"
   f << '## "\#{i}周年おめでとうございます" + ?！ * i'
   f << "\n"
   f << "---\n"
-  f << "# 30周年ということで、RubyでHello worldする30の方法を紹介します\n"
+  f << "# 30周年ということで、Rubyでハローワールドする30の方法を紹介します\n"
   f << "---\n"
   f << "# レギュレーション：\"Hello, world!\"を改行込みで出力する\n"
   f << "## 警告やエラーを出さない\n"
