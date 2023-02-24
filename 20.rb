c98965fc0f0333f22874631b3c050ee4ba093ae4
OKURA Masafumi <masafumi.o1988@gmail.com>
2023-02-24 17:45:58 +0900

    Improve method chain

--- 20.rb
+++ 20.rb
@@ -1,10 +1,11 @@
 # メソッドチェーンは便利
 class String
-  "Helloworld!".each_char do |char|
+  ("a".."z").each do |char|
     define_method(char) { self + char }
   end
   def comma = self + ?,
   def space = self + ' '
+  def ! = self + ?!
+  def puts = Kernel.puts(self)
 end
-
-puts "".H.e.l.l.o.comma.space.w.o.r.l.d.!
+"H".e.l.l.o.comma.space.w.o.r.l.d.!.puts
